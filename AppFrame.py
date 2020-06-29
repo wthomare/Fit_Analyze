f604226d956ece1caabda40625892bd4d6462a65
wthomare <wilfried.thomare@gmail.com>
2020-06-29 05:00:43 -0400

    Develop UI

--- AppFrame.py
+++ AppFrame.py
@@ -35,6 +35,17 @@ class RibbonFrame(wx.Frame):
         toolbar.AddTool(UI_parameters.ID_MNUFILEPRINT, wx.ArtProvider.GetBitmap(wx.ART_PRINT, wx.ART_OTHER, wx.Size(24,23)))
         toolbar.AddSeparator()
         toolbar.SetRows(2, 3)
+        
+        user_panel = RB.RibbonPanel(home, wx.ID_ANY, "User",style = RB.RIBBON_PANEL_NO_AUTO_MINIMISE | RB.RIBBON_PANEL_EXT_BUTTON)
+        user = RB.RibbonToolBar(user_panel, wx.ID_ANY)
+        user.AddTool(UI_parameters.ADD_User, wx.ArtProvider.GetBitmap(wx.ART_PLUS , wx.ART_OTHER, wx.Size(24,23)), "Add User")
+        user.AddTool(UI_parameters.DEL_User, wx.ArtProvider.GetBitmap(wx.ART_MINUS , wx.ART_OTHER, wx.Size(24,23)), "Delete User")
+        user.AddTool(UI_parameters.EDIT_User, wx.ArtProvider.GetBitmap(wx.ART_FIND_AND_REPLACE , wx.ART_OTHER, wx.Size(24,23)), "Modify User")
+
+        event_panel = RB.RibbonPanel(home, wx.ID_ANY, "Event",style = RB.RIBBON_PANEL_NO_AUTO_MINIMISE | RB.RIBBON_PANEL_EXT_BUTTON)
+        event = RB.RibbonToolBar(event_panel, wx.ID_ANY)
+        event.AddTool(UI_parameters.ADD_Event, wx.ArtProvider.GetBitmap(wx.ART_PLUS , wx.ART_OTHER, wx.Size(24,23)), "Add Event")
+        event.AddTool(UI_parameters.DEL_Event, wx.ArtProvider.GetBitmap(wx.ART_MINUS , wx.ART_OTHER, wx.Size(24,23)), "Delete Event")
 
 
         # Second page
@@ -73,6 +84,11 @@ class RibbonFrame(wx.Frame):
         
         # Print
         toolbar.Bind(RB.EVT_RIBBONTOOLBAR_CLICKED, self.OnMnuFilePrint, id=UI_parameters.ID_MNUFILEPRINT)
+
+        for ID in {UI_parameters.ADD_User, UI_parameters.DEL_User, UI_parameters.EDIT_User,
+                   UI_parameters.ADD_Event, UI_parameters.DEL_Event}:
+            self.Bind(RB.EVT_RIBBONTOOLBAR_CLICKED, (lambda x: self.OnNewAction(x)), id=ID)
+            
         
     def OnMnuUndo(self, event):
         pass
@@ -82,3 +98,8 @@ class RibbonFrame(wx.Frame):
     
     def OnMnuFilePrint(self, event):
         pass
+
+    def OnNewAction(self, event):
+        self._ctrl.setCurrentAction(UI_parameters.ACTIONS[event.GetId()])
+        self._fileHandling.setModified(True)
+        self._ctrl.updateTitle()
